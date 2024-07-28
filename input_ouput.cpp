9da9d023bcc3d89559a8a35fa249733a552bd6c7
Killy <heraishkumar123@gmail.com>
2024-07-29 01:12:34 +0530

    std::cin or std::cout does not work for some reason

---
+++ input_ouput.cpp
@@ -0,0 +1,13 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+#include <iostream>  
+
+
+int main() {
+    int test_var;
+    scanf("%d",&test_var);
+    printf("%d Hello World!" , test_var);
+    return 0;
+}
