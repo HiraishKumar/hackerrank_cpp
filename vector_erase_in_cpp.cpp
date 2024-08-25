2c1f031b20b67e3578e2ab3f82a64c17f5d072e2
Killy <heraishkumar123@gmail.com>
2024-08-25 15:37:07 +0530

    vector erase

---
+++ vector_erase_in_cpp.cpp
@@ -0,0 +1,33 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int len,loc;
+    cin >> len;
+    vector<int> arr(len); 
+    
+    for (auto& i:arr){
+        cin >> i;
+    }
+    cin >> loc;
+    arr.erase(arr.begin()+loc-1);
+    
+    int loc1,loc2;
+    cin >> loc1 >> loc2;
+    
+    arr.erase(arr.begin()+loc1-1,arr.begin()+loc2-1);
+    
+    cout << arr.size() << endl;
+    
+    for (auto i:arr){
+        cout << i << " ";
+    }
+    
+    return 0;
+}
